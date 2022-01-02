d92675fda8363a8d42698ada336afafd20fae45a
sufangmu <gaopeng0214@126.com>
2022-01-02 20:06:40 +0800

    创建模板

--- BugTracker/settings.py
+++ BugTracker/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app.apps.AppConfig',
+    'web.apps.WebConfig',
 ]
 
 MIDDLEWARE = [
@@ -114,8 +114,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 try:
     from .dev_settings import *
-except Exception:
+except ImportError:
     pass
