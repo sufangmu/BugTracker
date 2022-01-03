ad79548a46290f09d09e5390e710271d17211031
sufangmu <gaopeng0214@126.com>
2022-01-03 19:01:42 +0800

    前端页面获取手机号，并发送到后端

--- web/urls.py
+++ web/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 from web.views import account
 urlpatterns = [
     path('register/', account.register, name='register'),
+    path('send_sms/', account.send_sms, name='send_sms'),
 ]
--- web/views/account.py
+++ web/views/account.py
@@ -1,7 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from web.forms.account import RegisterModelForm
 
 
 def register(request):
+    """注册"""
     form = RegisterModelForm()
     return render(request, 'register.html', {'form': form})
+
+
+def send_sms(request):
+    """发送短信验证码"""
+    if request.is_ajax():
+        print(request.POST)
+    return HttpResponse("ok")
