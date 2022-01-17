b64f2a52602905a11221193999c56c2e81932f62
sufangmu <gaopeng0214@126.com>
2022-01-17 21:19:14 +0800

    1. 获取星标项目，创建的项目，参与的项目 2. 前端展示上述项目

--- web/views/project.py
+++ web/views/project.py
@@ -4,12 +4,37 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from web.forms.project import ProjectModelForm
+from web import models
 
 
 def project_list(request):
+    """项目列表"""
+    # 1. 从数据库中获取两部分数据
+    #     我创建的所有项目：已星标、未星标
+    #     我参与的所有项目：已星标、未星标
+    # 2. 提取已星标
+    #     列表 = 循环 [我创建的所有项目] + [我参与的所有项目] 把已星标的数据提取
+    #
+    # 得到三个列表：星标、创建、参与
     if request.method == "GET":
+        user = request.tracker.user
+        project_dict = {"star": [], "mine": [], "join": []}
+        # 获取我创建的项目
+        my_project_list = models.Project.objects.filter(creator=user)
+        for item in my_project_list:
+            if item.star:
+                project_dict["star"].append(item)
+            else:
+                project_dict["mine"].append(item)
+        # 获取我参与的项目
+        join_project_list = models.ProjectUser.objects.filter(user=user)
+        for item in join_project_list:
+            if item.star:
+                project_dict["star"].append(item.project)
+            else:
+                project_dict["join"].append(item.project)
         form = ProjectModelForm(request)
-        return render(request, 'project_list.html', {"form": form})
+        return render(request, 'project_list.html', {"form": form, "projects": project_dict})
     if request.method == "POST":
         form = ProjectModelForm(request, data=request.POST)
         if form.is_valid():
