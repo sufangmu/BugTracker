6d2a0b528cfa2a57c3de66c0a19cf966f682e9d5
sufangmu <gaopeng0214@126.com>
2022-01-22 17:50:48 +0800

    1. 利用inclusion_tag遍历菜单列表 2. 给访问的url设置选中样式

--- web/templatetags/project.py
+++ web/templatetags/project.py
@@ -3,6 +3,7 @@
 # filename: project.py
 from django import template
 from web import models
+from django.urls import reverse
 
 register = template.Library()
 
@@ -15,3 +16,20 @@ def all_project_list(reqeust):
     # 获取我参与的所有项目
     join = models.ProjectUser.objects.filter(user=user)
     return {"mine": mine, "join": join}
+
+
+@register.inclusion_tag('inclusion/manage_menu_list.html')
+def manage_menu_list(request):
+    menu_list = [
+        {"title": "概览", "url": reverse("dashboard", kwargs={"project_id": request.tracker.project.id})},
+        {"title": "问题", "url": reverse("issue", kwargs={"project_id": request.tracker.project.id})},
+        {"title": "统计", "url": reverse("statistics", kwargs={"project_id": request.tracker.project.id})},
+        {"title": "文件", "url": reverse("file", kwargs={"project_id": request.tracker.project.id})},
+        {"title": "wiki", "url": reverse("wiki", kwargs={"project_id": request.tracker.project.id})},
+        {"title": "设置", "url": reverse("setting", kwargs={"project_id": request.tracker.project.id})},
+    ]
+    for menu in menu_list:
+        if request.path_info.startswith(menu["url"]):
+            menu["class"] = "active"
+
+    return {"menu_list": menu_list}
