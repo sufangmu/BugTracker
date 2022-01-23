bc76637a754500341cc158ca8373cc5fb60bb6d9
sufangmu <gaopeng0214@126.com>
2022-01-23 10:28:19 +0800

    1. 前端展示一级文档（没有付文档的）

--- web/views/wiki.py
+++ web/views/wiki.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 # _*_ coding:utf-8 _*_
 # filename: wiki.py
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
+from web import models
 from web.forms.wiki import WikiModelForm
 
 
@@ -20,3 +22,9 @@ def wiki_add(request, project_id):
             form.save()
             return redirect(reverse('wiki', kwargs={"project_id": project_id}))
     return render(request, "wiki_add.html", {"form": form})
+
+
+def wiki_catalog(request, project_id):
+    """wiki目录"""
+    catalog = models.Wiki.objects.filter(project_id=project_id).values("id", "title", "parent")
+    return JsonResponse({"status": True, "data": list(catalog)})
