7c0b45ebd7d93031427a1401c7e51a8835e555f5
sufangmu <gaopeng0214@126.com>
2022-03-23 19:28:07 +0800

    1. 完成添加问题模态框的错误提示 2. 问题添加成功保存至数据库并刷新页面

--- web/views/issues.py
+++ web/views/issues.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from web.forms.issues import IssuesForm
+from django.http import JsonResponse
 
 
 def issue(request, project_id):
     form = IssuesForm(request)
+    if request.method == "POST":
+        form = IssuesForm(request, data=request.POST)
+        if form.is_valid():
+            form.instance.project = request.tracker.project
+            form.instance.creator = request.tracker.user
+            form.save()
+            return JsonResponse({"status": True})
+        return JsonResponse({"status": False, "error": form.errors})
     return render(request, 'issue.html', {"form": form})
