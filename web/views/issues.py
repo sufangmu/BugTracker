db6e56fe9c743a453390568158d510c8f878a0a2
sufangmu <gaopeng0214@126.com>
2022-04-03 20:52:36 +0800

    1. 问题列表分页

--- web/views/issues.py
+++ web/views/issues.py
@@ -2,13 +2,24 @@ from django.shortcuts import render
 from web.forms.issues import IssuesForm
 from django.http import JsonResponse
 from web import models
+from web.utils.pagination import Pagination
 
 
 def issue(request, project_id):
     if request.method == "GET":
+        queryset = models.Issues.objects.filter(project_id=project_id)
+        page_obj = Pagination(
+            current_page=request.GET.get('page'),
+            all_count=queryset.count(),
+            base_url=request.path_info,
+            query_params=request.GET,
+            per_page=10,
+        )
         form = IssuesForm(request)
-        issues_obj_list = models.Issues.objects.filter(project_id=project_id)
-        return render(request, 'issue.html', {"form": form, "issues": issues_obj_list})
+        issues_obj_list = queryset[page_obj.start:page_obj.end]
+
+        return render(request, 'issue.html',
+                      {"form": form, "issues": issues_obj_list, "page_html": page_obj.page_html()})
 
     if request.method == "POST":
         form = IssuesForm(request, data=request.POST)
